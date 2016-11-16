e719a6b3ad4aace7b5c928ad9f1275e54a194c38
Mauro Donega <mauro.donega@cern.ch>
2016-11-16 10:16:32 +0100

    train on any Y in the ntuple

--- MTR/runna.sh
+++ MTR/runna.sh
@@ -2,22 +2,29 @@
 
 # qsub all in steps
 
-#k=0.025
-#for i in `seq 1 39`;
-#do
-#    # q=`echo "$k*$i"|bc`
-#    qsub jobba.sh "mc"   $q 0 1500000 -q short.q
-#    qsub jobba.sh "data" $q 0 -1 -q short.q 
-#done
+#  "R9", "S4", "SigmaIeIe", "EtaWidth", "PhiWidth", "CovarianceIphiIphi", "SigmaRR"]
+
+k=0.025
+
+for i in `seq 1 1`;
+do
+    q=`echo "$k*$i"|bc`
+
+    qsub jobba.sh "mc"   R9 $q 0 1500000 -q short.q
+    qsub jobba.sh "data" R9 $q 0 -1 -q short.q 
+
+    qsub jobba.sh "mc"   SigmaIeIe $q 0 1500000 -q short.q
+    qsub jobba.sh "data" SigmaIeIe $q 0 -1 -q short.q 
+done
 
 
 # re-run only a few points
-for i in 0.150 0.375;
-do
-    echo $i
-    qsub jobba.sh "mc"   $i 0 1500000 -q short.q
-done    
-
+#for i in 0.98;
+#do
+#    echo $i
+#
+#done    
+#
 
 
 
