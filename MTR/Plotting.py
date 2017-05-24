e7f9acfd66cdc64bab376d920353e194184f3699
giulioisac <giulioisac@gmail.com>
2017-05-24 18:38:17 +0200

    first figure

--- MTR/Plotting.py
+++ MTR/Plotting.py
@@ -172,3 +172,11 @@ def Profile(x,y,nbins,xmin,xmax,ax, color,weight=1):
     ProfileFrame['yMeanError'] = ProfileFrame['yMeanError'].fillna(0)
     ax.errorbar(ProfileFrame['bincenters'], ProfileFrame['ymean'], yerr=np.array(ProfileFrame['yMeanError']), c=color,capsize=0)
     return ax
+
+def exportToPdf(name='sample',section='sample',mode='a'):
+    f = open('Plots/'+section+'.tex', mode)
+    f.write('\\begin{figure}[H]')
+    f.write('\\begin{center}')
+    f.write('\\includegraphics[width=15.0cm]{'+name+'.png}')
+    f.write('\\end{center}')
+    f.write('\\end{figure}')
